f7c5f5b55e75e769c5564d7350bf2128ec2235c9
shimaTaka <takanori23@hotmail.co.jp>
2018-06-24 00:05:08 +0900

    Add-Migration Update-Database 実施

---
+++ StudyWpf/Models/TestContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyWpf.Models
+{
+    internal class TestContext : DbContext
+    {
+
+        public DbSet<TestModel> Items { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = new SqliteConnectionStringBuilder { DataSource = @"C:\temp\Test.db" }.ToString();
+            optionsBuilder.UseSqlite(new SqliteConnection(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestModel>().HasKey(key => new { key.Id, key.SheetId});
+        }
+
+    }
+}
---
+++ StudyWpf/Models/TestModel.cs
@@ -0,0 +1,15 @@
+﻿namespace StudyWpf.Models
+{
+    public class TestModel
+    {
+        public enum ResultStatusDefinition {
+            OK,
+            NG,
+            NotYet,
+        }
+        public int Id { get; internal set; }
+        public int SheetId { get; internal set; }
+        public string Name { get; set; }
+        public ResultStatusDefinition ResultStatus { get; set; }
+    }
+}
